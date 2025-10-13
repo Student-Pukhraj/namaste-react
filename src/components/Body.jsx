8c15db3484eefbf134a0aa56b727d39cad7b7eed
Pukhraj Jharotiya <pukhrajjharotiya15@gmail.com>
2025-10-13 07:16:06 +0530

    Refactor Body component to fetch restaurant data from API; update state initialization and implement useEffect for data retrieval.
    Signed-off-by: Pukhraj Jharotiya <pukhrajjharotiya15@gmail.com>

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8191641&lng=75.7659513&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+    );
+    const json = await data.json();
+    console.log(json.data.cards[3].card.card.info);
+    setListOfRestaurants(json.data.cards[3].card.card.info);
+  };
+
+  console.log("Body rendered");
   return (
     <div className="body">
       <div className="filter">
